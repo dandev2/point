1fba9df46072c80b317a18c35c80c2c5d85ede4a
dandev <dandev@naver.com>
2022-08-01 16:49:47 +0900

    data class 생성

--- src/main/java/com/example/musinsa/data/Member.java
+++ src/main/java/com/example/musinsa/data/Member.java
@@ -1,12 +1,49 @@
 package com.example.musinsa.data;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Member {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
+
     @Column
-    private int point;
+    private String name; // 회원이름
+
+    @Column(name="pointname")
+    private String pointName; //포인트 이름
+
+    @CreatedDate
+    @Column(name = "regdate", updatable = false)
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    @Column(name="moddate")
+    private LocalDateTime modDate;
+
+    @Column(nullable = false, length = 10)
+    private int point; // 포인트
+
+    @Column(name="balancingpoint", length=10)
+    private int balancingPoint; // 잔액
+
+    @Column(name="totalpoint", length = 10)
+    private int totalPoint; // 총액
+
+    // 관리자가 포인트 금액 수정할 수 있게 설정
+    public void changePoint(int point) {
+        this.point = point;
+    }
 
 }
